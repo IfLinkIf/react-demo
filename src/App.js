8614f79a63d02556c4a602119c4f2365184fa6d5
jackchain <ovenjackchain@gmail.com>
2023-05-16 20:35:21 +0800

    react demo init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Layout from './pages/layout'
+import Login from './pages/login'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/layout/index.js
@@ -0,0 +1,10 @@
+
+function Layout () {
+ return (
+  <div>
+   <h1>Layout</h1>
+  </div>
+ )
+}
+
+export default Layout
